ed4a4ec46707e0ac0fbc9faa5011e7ce8dd6c7de
davitkhanal <khanaldavit@gmail.com>
2022-03-28 17:47:17 +0545

    HHTp server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-const
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const app = express()
+
+const PORT =process.env.PORT
+
+app.get('/', (req,res)=>{
+    res.send("Crud Application")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server has started in ${PORT}`)
+})
