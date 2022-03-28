fd0aa0b747ab2ffcb2b3b0b35975fdc8082be155
davitkhanal <khanaldavit@gmail.com>
2022-03-28 17:54:00 +0545

    ejs engines added

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const morgan = require('morgan')
+const bodyparser = require("body-parser")
+const path = require("path")
 
 const app = express()
-
 const PORT =process.env.PORT
 
+//log request
+app.use(morgan("tiny"))
+
+//parse request 
+app.use(bodyparser.urlencoded({ extended:true}))
+
+//set view engine
+app.set("view engine", "ejs")
+//app.set("views",path.resolve(__dirname, "views/ejs"))
+
+
+//loading assets
+app.use('/css', express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img', express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
+
+//getting and listening
 app.get('/', (req,res)=>{
     res.send("Crud Application")
 })
