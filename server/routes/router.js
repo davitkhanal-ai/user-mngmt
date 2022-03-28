741e05d05991045f1bcbd72895ae830614f3402b
davitkhanal <khanaldavit@gmail.com>
2022-03-28 19:31:08 +0545

    routing for add / update

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const route = express.Router()
+const services = require("../services/render")
 
-route.get('/', (req,res)=>{
-    res.render('index');
-})
 
-route.get('/add-user', (req,res)=>{
-    res.render('add_user');
-})
+/** @description Root route
+ * @method Get
+ */
+route.get('/',services.homeRoutes)
 
-route.get('/update-user', (req,res)=>{
-    res.render('update_user');
-})
+/** @description add user
+ * @method Post
+ */
+route.get('/add-user',services.add_user)
+
+/** @description update user
+ * @method Get
+ */
+route.get('/update-user',services.update_user)
+
 
 module.exports = route
