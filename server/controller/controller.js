c2646c7a5698b3a1a97b59f7e23862766aafbf05
davitkhanal <khanaldavit@gmail.com>
2022-03-28 20:06:37 +0545

    create user method

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,35 @@
-var userdb = require("../models/model")
+const Userdb = require("../models/model");
+
 
 
 //create and save user
 exports.create = (req,res)=>{
+    //validate the request
+    if(!req.body){
+        res.status(400).send({ message:"content cannot be empty!"})
+        return;
+    }
+
+    //new user
+    const user = new Userdb({
+        name: req.body.name,
+        email: req.body.email,
+        number: req.body.number,
+        gender: req.body.gender,
+        status: req.body.status
+    })
+
+    //saving data
+    user
+        .save(user)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err =>{
+            res.status(500).send({ 
+                message: err.message || "error occured"
+            })
+        })
 
 }
 
